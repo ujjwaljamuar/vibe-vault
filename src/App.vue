cdd3d91f1e016e25bbeea743b4527760630f46f2
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-18 20:55:24 +0530

    added table for all items

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
-    <HomePage msg="VibeVault" />
-</template>
-
-<script>
-import HomePage from "./components/Home.vue";
-
-export default {
-    name: "App",
-    components: {
-        HomePage,
-    },
-};
-</script>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
+    <v-app>
+      <v-container>
+        <HomePage />
+      </v-container>
+    </v-app>
+  </template>
+  
+  <script setup>
+  import HomePage from './components/Homepage.vue';
+  
+  
+  </script>
+  
+  <style>
+  /* Global styles if needed */
+  </style>
+  
+  
+  
---
+++ src/components/Homepage.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <h1>VibeVault</h1>
+        <div>
+            <MusicTable />
+        </div>
+    </v-container>
+</template>
+
+<script setup>
+import MusicTable from "./MusicTable.vue";
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+
+</style>
