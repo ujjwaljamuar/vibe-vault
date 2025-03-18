9730614c4f2d0c86f1a80f5bf2f2a0799622e2ff
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-18 13:25:10 +0530

    migrated to vue from nextjs

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TableDemo } from "./components/Table";
-
-export default function Home() {
-  return (
-    <div style={{maxHeight: "95dvh", maxWidth: "95dvw",display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <TableDemo />
-    </div>
-  );
-}
